7b049bdb717ea10246de047ff713e5398b4abfdc
jgrissom <jgrissom91@gmail.com>
2020-09-23 17:32:52 -0500

    Album class - Create the Album class - it is a subclass of the Media class - Add artist and recordLabel properties to the Album class - Override the base class’s Display method in the Album class - Create an instance of the Album class in the Program and display it

--- Media.cs
+++ Media.cs
@@ -22,7 +22,7 @@ namespace MediaLibrary
             return $"Id: {mediaId}\nTitle: {title}\nGenres: {string.Join(", ", genres)}\n";
         }
     }
-    
+
     // Movie class is derived from Media class
     public class Movie : Media
     {
@@ -32,6 +32,18 @@ namespace MediaLibrary
         public override string Display()
         {
             return $"Id: {mediaId}\nTitle: {title}\nDirector: {director}\nRun time: {runningTime}\nGenres: {string.Join(", ", genres)}\n";
+        }
+    }
+
+    // Album class is derived from Media class
+    public class Album : Media
+    {
+        public string artist { get; set; }
+        public string recordLabel { get; set; }
+
+        public override string Display()
+        {
+            return $"Id: {mediaId}\nTitle: {title}\nArtist: {artist}\nLabel: {recordLabel}\nGenres: {string.Join(", ", genres)}\n";
         }
     }
 }
